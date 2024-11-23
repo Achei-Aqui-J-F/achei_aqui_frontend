6a8e235c008a73f108befe419e63aac0e9de332c
Hideki1202 <matheus.ueno@germinare.org.br>
2024-11-23 18:33:33 -0300

    validators implementados

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -6,6 +6,7 @@ import { cityViewModel } from './view-models/city-vm';
 import { adressViewModel } from './view-models/adress-vm';
 import { switchMap } from 'rxjs/operators';
 import { UserViewModel } from './view-models/user-vm';
+import { BlobOptions } from 'buffer';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -14,19 +15,22 @@ import { UserViewModel } from './view-models/user-vm';
 export class SignUpComponent {
   
   cadastroForm = new FormGroup({
-    nome: new FormControl('', ),
-    sobrenome: new FormControl('',),
-    email: new FormControl('', ),
-    telefone: new FormControl('',),
-    senha: new FormControl('', ),
-    cep: new FormControl('',),
-    estado: new FormControl('', ),
-    cidade: new FormControl('',),
-    bairro: new FormControl('',),
-    rua: new FormControl('', ),
-    uf: new FormControl('', ),
-
+    nome: new FormControl('', Validators.required),
+    sobrenome: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl('', Validators.required),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8), // Senha com pelo menos 8 caracteres
+      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}') // Letras maiúsculas, minúsculas e números
+    ]),
+    cep: new FormControl('', Validators.required),
+    cidade: new FormControl('', Validators.required),
+    bairro: new FormControl('', Validators.required),
+    rua: new FormControl('', Validators.required),
+    uf: new FormControl('', Validators.required),
   });
+  passwordIsValid: boolean = true;
   states:stateViewModel[] = []
   cities:cityViewModel[] = []
   adress:adressViewModel = {city:"", district:"", state:"", street:""}
@@ -61,24 +65,37 @@ export class SignUpComponent {
 
   onSubmit(){
     console.log("SUBMIT")
-    this.user = {
-      nome: this.cadastroForm.value.nome + " " + this.cadastroForm.value.sobrenome,
-      senha: this.cadastroForm.value.senha || "",
-      email: this.cadastroForm.value.email || "",
-      telefone: this.cadastroForm.value.telefone || "",
-      endereco: {
-        estado: this.cadastroForm.value.uf || "",
-        cep: this.cadastroForm.value.cep || "",
-        bairro: this.cadastroForm.value.bairro || "",
-        rua: this.cadastroForm.value.rua || "",
-        cidade: this.cadastroForm.value.cidade || "",
-      },
-    };
-    console.log('Dados enviados:', this.user);
-    this.service.createUser(this.user).subscribe({
-      next: (response) => console.log('Usuário criado com sucesso:', response),
-      error: (err) => console.error('Erro na criação do usuário:', err),
-    });
+    if(this.cadastroForm.valid){
+      console.log("VALIDO")
+      this.user = {
+        nome: this.cadastroForm.value.nome + " " + this.cadastroForm.value.sobrenome,
+        senha: this.cadastroForm.value.senha || "",
+        email: this.cadastroForm.value.email || "",
+        telefone: this.cadastroForm.value.telefone || "",
+        endereco: {
+          estado: this.cadastroForm.value.uf || "",
+          cep: this.cadastroForm.value.cep || "",
+          bairro: this.cadastroForm.value.bairro || "",
+          rua: this.cadastroForm.value.rua || "",
+          cidade: this.cadastroForm.value.cidade || "",
+        },
+      };
+      console.log('Dados enviados:', this.user);
+      this.service.createUser(this.user).subscribe({
+        next: (response) => console.log('Usuário criado com sucesso:', response),
+        error: (err) => console.error('Erro na criação do usuário:', err),
+      });
+    }else{
+      console.log("INVALIDO")
+
+      const senhaControle = this.cadastroForm.get('senha');
+      if (senhaControle && senhaControle.invalid) {
+        this.passwordIsValid = false
+      }else{
+          this.passwordIsValid = true
+      }
+    }
+    
   }
   onBlurCEP(){
     console.log("CEP DIGITADO")
